9ddcbe842864524724fe70de9769d466bb6ca550
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-18 13:39:03 +0530

    [Abhay Shrivastava] Refactor . Improved CSV reading logic with OpenCSV

--- Day01-CSV_DataHandling/src/main/java/com/io_programming/BasicProblems/Read_CSV/ReadCSV_File.java
+++ Day01-CSV_DataHandling/src/main/java/com/io_programming/BasicProblems/Read_CSV/ReadCSV_File.java
@@ -3,12 +3,13 @@ package com.io_programming.BasicProblems.Read_CSV;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import com.opencsv.CSVReader;
 
 public class ReadCSV_File {
 
     public static void main(String[] args) {
         String filepath = "src/main/java/com/io_programming/BasicProblems/Read_CSV/students.csv";
-
+        /*
         try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
             String line;
             while((line = br.readLine()) != null){
@@ -17,6 +18,23 @@ public class ReadCSV_File {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
+        }
+        */
+
+        //Reading csv file using opencsv
+        try(CSVReader csvReader = new CSVReader(new FileReader(filepath))){
+               String[] rowData;
+
+               while((rowData = csvReader.readNext()) != null){
+                   System.out.println("ID: " + rowData[0] + ", Name: " + rowData[1] + ", Age: " + rowData[2] + ", Marks: " + rowData[3]);
+               }
+
+        } catch (RuntimeException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("General error: " + e.getMessage());
         }
     }
 }
