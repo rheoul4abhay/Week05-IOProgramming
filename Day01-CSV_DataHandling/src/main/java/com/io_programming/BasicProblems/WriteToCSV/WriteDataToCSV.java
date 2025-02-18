f8dc4a24ee3e9fcd68cae4cdb4c3502192d11d72
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-18 14:41:24 +0530

    [Abhay Shrivastava] Refactor . Improved CSV writing logic with OpenCSV

--- Day01-CSV_DataHandling/src/main/java/com/io_programming/BasicProblems/WriteToCSV/WriteDataToCSV.java
+++ Day01-CSV_DataHandling/src/main/java/com/io_programming/BasicProblems/WriteToCSV/WriteDataToCSV.java
@@ -1,5 +1,7 @@
 package com.io_programming.BasicProblems.WriteToCSV;
 
+import com.opencsv.CSVWriter;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,6 +12,7 @@ public class WriteDataToCSV {
         String destination_path = "src/main/java/com/io_programming/BasicProblems/WriteToCSV/employees.csv";
 
         //Using BufferedWriter to write into destination
+        /*
         try(BufferedWriter bfw = new BufferedWriter(new FileWriter(destination_path))){
             bfw.write("ID, Name, Department, Salary\n");
             bfw.write("101, Abhay, Sales, 500.0\n");
@@ -17,6 +20,25 @@ public class WriteDataToCSV {
             bfw.write("103, Reyna, Management, 400.0\n");
             bfw.write("104, Mikey, Consultancy, 350.0\n");
             System.out.println("CSV file written successfully!");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        */
+
+        //Write Using openCSV
+        try(CSVWriter csvWriter = new CSVWriter(new FileWriter(destination_path))){
+            String[] header = {"ID", "Name", "Department", "Salary"};
+            String[] emp1 = {"101", "Abhay", "Sales", "2500.0"};
+            String[] emp2 = {"102", "Samay", "Accounts", "1500.0"};
+            String[] emp3 = {"103", "Reyna", "Management", "3500.0"};
+            String[] emp4 = {"104", "Mikey", "Consultancy", "500.0"};
+
+            csvWriter.writeNext(header);
+            csvWriter.writeNext(emp1);
+            csvWriter.writeNext(emp2);
+            csvWriter.writeNext(emp3);
+            csvWriter.writeNext(emp4);
+            System.out.println("CSV file written successfully.");
         } catch (IOException e){
             e.printStackTrace();
         }
