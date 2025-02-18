3e1c9e52fe82adfdb1a5f289a862105718385d6e
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2025-02-18 16:13:28 +0530

    [Abhay Shrivastava] Refactor . added filter stream to filter data from csv"

--- Day01-CSV_DataHandling/src/main/java/com/io_programming/IntermediateProblems/FilterRecords/FilterCSV_Data.java
+++ Day01-CSV_DataHandling/src/main/java/com/io_programming/IntermediateProblems/FilterRecords/FilterCSV_Data.java
@@ -5,12 +5,14 @@ import com.opencsv.CSVReaderBuilder;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilterCSV_Data {
     public static void main(String[] args) {
         String filepath = "src/main/java/com/io_programming/IntermediateProblems/FilterRecords/students.csv";
 
         try(CSVReader csvReader = new CSVReaderBuilder(new FileReader(filepath)).withSkipLines(1).build()){
+            /*
             List<String[]> allData = csvReader.readAll();
             for(String[] row: allData){
                 boolean recordFound = false;
@@ -22,6 +24,11 @@ public class FilterCSV_Data {
                 }
                 if(recordFound) System.out.println();
             }
+             */
+
+            //Using filter stream
+            List<String[]> filteredData = csvReader.readAll().stream().filter(row -> Integer.parseInt(row[3]) > 80).collect(Collectors.toList());
+            filteredData.forEach(row -> System.out.println(String.join(" ", row)));
 
         } catch (IOException e) {
             e.printStackTrace();
